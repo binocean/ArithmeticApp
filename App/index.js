dc201c5d16abeea950ccc0cf10b85f3915c65830
binocean <1328463287@qq.com>
2020-10-11 15:43:14 +0800

    加入弹窗功能

--- App/index.js
+++ App/index.js
@@ -1,5 +1,7 @@
 const $ = require('jquery')
-
+const {
+  dialog
+} = require('electron').remote;
 
 let $questionNumber = $('#questions-number'); // 题目总数input
 let $range = $('#range'); // 题目范围input
@@ -12,7 +14,22 @@ let $collateBtn = $('#collate'); // 点击校对答案的按钮
 
 // 生成题目按钮的点击
 $creatBtn.on('click', function() {
-  console.log('生成题目123');
+  if (/^\d+$/.test($questionNumber.val()) && /^\d+$/.test($range.val())) {
+    dialog.showMessageBox({
+      type: 'info',
+      title: '生成完毕',
+      message: '已在文件根目录生成Exercises.txt以及Answers.txt文件',
+      buttons: ['好的']
+    })
+  } else {
+    dialog.showMessageBox({
+      type: 'error',
+      title: '参数有误',
+      message: '请检查输入的参数是否为自然数',
+      buttons: ['好的']
+    })
+  }
+  
 })
 
 $questionUpload.on('click', function() {
